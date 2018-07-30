03cb00abbbb6e7ce52c518eb0cf3bdca42c52da9
Ashutosh Varma <ashutoshvarma11@live.com>
2018-07-30 22:39:07 +0530

    convert to module

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='musicutil',
+    version='0.1.2-alpha1',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'beautifulsoup4','requests', 'html5lib', 'spotipy'
+    ],
+)
